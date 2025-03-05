1e579b5f513e4285e6d4aabbb75a49bcfeea6a32
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-03-06 01:56:44 +0500

    Update the code

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import Card from "../Cards/Card"
+import mern from "../../Assets/mern.png"
 import "./Project.css"
 function Project() {
   return (
     <div id="projects">
-        <h1>References available upon request.</h1>
+        <h1 id='para'>References available upon request.</h1>
         <div className="slider">
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+          <Card title="Stopwatch" image={mern}/>
+          <Card title="Stopwatch" image={mern}/>
+          <Card title="Stopwatch" image={mern}/>
+          <Card title="Stopwatch" image={mern}/>
+          <Card title="Stopwatch" image={mern}/>
         </div>
     </div>
   )
