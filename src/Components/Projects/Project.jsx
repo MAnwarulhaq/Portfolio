0793fea673503b79bb53b73fdd5a0312e348e371
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-03-06 00:57:56 +0500

    Update the code

---
+++ src/Components/Projects/Project.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Project() {
+  return (
+    <div className="project">
+        <h1></h1>
+        <div className="slider">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Project
