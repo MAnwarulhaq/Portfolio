1d6a59002c88b74b6b5cc316e70a6c2ef333d0a9
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-03-22 00:45:53 +0500

    udate the ecommerce project picture

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Card from "../Cards/Card";
 import mern from "../../Assets/mern.png";
+import ecommecre from "../../Assets/Ecommerce.png";
 import { Link } from "react-router-dom";
 import "./Project.css";
 import gsap from 'gsap';
@@ -40,7 +41,7 @@ function Project() {
     <div id="projects">
       <h1 id="para">References available upon request.</h1>
       <div className="slider">
-        <Link to="https://buymoree.netlify.app/"><Card title="E-Commerce" image={mern} /></Link>
+        <Link to="https://buymoree.netlify.app/"><Card title="E-Commerce" image={ecommecre} /></Link>
         <Link> <Card title="Weather App" image={mern} /></Link>
         <Link><Card title="Portfolio Website" image={mern} /></Link>
         <Link><Card title="E-commerce Store" image={mern} /></Link>
