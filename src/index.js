064ebcbaa4313cd158931feaa80a93723cf4dd74
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2026-01-09 20:43:23 +0500

    update the code

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>   {/* 👈 Router wrap kare */}
+    <BrowserRouter>  
       <App />
     </BrowserRouter>
   </React.StrictMode>
